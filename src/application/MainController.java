252cb9eb2a964fc979d60b68a51b531adb2f65c1
heyitschrislol <heyitschrislol@users.noreply.github.com>
2020-09-20 12:05:39 -0500

    jordan makes me butt

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.StackPane;
 public class MainController {
 /** Holder of a switchable pane. */
 	// poopypop
+    // jordan makes me so butt
     @FXML
     private StackPane viewHolder;
 
